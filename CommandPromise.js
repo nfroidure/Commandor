d9122daba3cfe26bd936d8a75eaa5f1fe7d1faab
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-07-11 14:51:39 +0200

    Added CommandPromise

---
+++ CommandPromise.js
@@ -0,0 +1,38 @@
+// AMD + Global: r.js compatible
+// Use START + END markers to keep module content only
+(function(root,define){ define(['./../promise/Promise'], function(Promise) {
+// START: Module logic start
+
+	// XHR promise constructor
+	function CommandPromise(commandor,command,view) {
+		if(!(this instanceof CommandPromise))
+			throw Error('Use new to intantiate !');
+		Promise.call(this,function(success) { console.log('register: '+view+'/'+command);
+			commandor.suscribe(view+'/'+command,function(event, params, element) {
+				dispose();
+				success({'event':event, 'params':params, 'element': element });
+
+			});
+			var dispose=function() { console.log('unregister: '+view+'/'+command);
+				commandor.unsuscribe(view+'/'+command);
+			};
+			return dispose;
+		});
+		this.command=command;
+	}
+
+	CommandPromise.prototype=Object.create(Promise.prototype);
+
+// END: Module logic end
+
+	return CommandPromise;
+
+});})(this,typeof define === 'function' && define.amd ? define : function (name, deps, factory) {
+	var root=this;
+	if(typeof name === 'object') {
+		factory=deps; deps=name; name='CommandPromise';
+	}
+	this[name.substring(name.lastIndexOf('/')+1)]=factory.apply(this, deps.map(function(dep){
+		return root[dep.substring(dep.lastIndexOf('/')+1)];
+	}));
+}.bind(this));
