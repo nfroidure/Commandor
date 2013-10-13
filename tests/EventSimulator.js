9cabc88d37541005f6decd4e2bfdaf3e42685f0a
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-10-13 15:23:43 +0200

    Improved the event simulator

--- tests/EventSimulator.js
+++ tests/EventSimulator.js
@@ -44,7 +44,7 @@
 				event.metaKey = options.metaKey;
 			} catch(e) {
 				event = document.createEvent('MouseEvent');
-				event.initMouseEvent(options.type||'click',
+				event.initMouseEvent(options.type,
 					'false' === options.canBubble ? false : true,
 					'false' === options.cancelable ? false : true,
 					options.view,
@@ -76,16 +76,23 @@
 	}
 
 	function click(element,options) {
+		var dispatched;
 		options=options||{};
 		options.type='mousedown';
-		mouse(element, options);
+		dispatched=mouse(element, options);
 		options.type='mouseup';
-		mouse(element, options);
+		if(!(mouse(element, options)&&dispatched)) {
+			return false;
+		}
 		options.type='click';
-		mouse(element, options);
+		return mouse(element, options);
 	}
 
 	// Pointer interactions
+	function pointerIsEnabled() {
+		return window.navigator.msPointerEnabled ? true : false;
+	}
+
 	function pointer(element,options) {
 		options=options||{};
 		var event = document.createEvent('MSPointerEvent');
@@ -105,18 +112,28 @@
 			options.tiltX||0, options.tiltY||0,
 			options.pointerId||1, options.pointerType||'mouse',
 			options.hwTimestamp||Date.now(), options.isPrimary||true);
-		element.dispatchEvent(event);
+		return element.dispatchEvent(event);
 	}
 
 	function point(element,options) {
 		options=options||{};
 		options.type='MSPointerDown';
-		pointer(element,options);
+		dispatched=pointer(element,options);
+		// Should detect IE10 and trigger the click event even if the pointer
+		// event is cancelled. IE11+ fixed the issue.
 		options.type='MSPointerUp';
-		pointer(element,options);
+		if(!(pointer(element,options)&&dispatched)) {
+			return false;
+		}
+		options.type='click';
+		return mouse(element, options);
 	}
 
 	// Tactile interactions
+	function tactileIsEnabled() {
+		return !!('ontouchstart' in window);
+	}
+
 	function tactile(element,options) {
 		options=options||{};
 		var event = document.createEvent('UIEvent');
@@ -130,18 +147,24 @@
 		event.ctrlKey = !!options.ctrlKey;
 		event.shiftKey = !!options.shiftKey;
 		event.metaKey = !!options.metaKey;
-		element.dispatchEvent(event);
+		return element.dispatchEvent(event);
 	}
 
 	function touch(element,options) {
+		var dispatched;
 		options=options||{};
 		options.type='touchstart';
-		tactile(element, options);
+		dispatched=tactile(element, options);
 		options.type='touchend';
-		tactile(element, options);
+		if(!(tactile(element, options)&&dispatched)) {
+			return false;
+		}
+		options.type='click';
+		return mouse(element, options);
 	}
 
-	// Keyboard interactions
+	// Keyboard interactions : lack of a keymap between various keyCode/charCode
+	// in different browsers
 	function keyboard(element,options) {
 		var event;
 		options=options||{};
@@ -200,30 +223,34 @@
 				event.char=String.fromCharCode(options.charCode);
 				event.ctrlKey=options.ctrlKey;
 			}
-			element.dispatchEvent(event);
+			return element.dispatchEvent(event);
 		} else if(document.createEventObject) {
 			event = document.createEventObject();
 			event.eventType=options.type;
 			event.keyCode=options.keyCode;
 			event.charCode=options.charCode;
-      element.fireEvent('on'+options.type, event);
+      return element.fireEvent('on'+options.type, event);
 		}
 	}
 
 	function type(element,options) {
+		var dispatched;
 		options=options||{};
 		options.type='keydown';
-		keyboard(element, options);
-		if(options.keyCode&&String.fromCharCode(options.keyCode)
+		dispatched=keyboard(element, options);
+		if(dispatched&&options.keyCode
+			&&String.fromCharCode(options.keyCode)
 			&&('TEXTAREA'===element.nodeName
 			||('INPUT'===element.nodeName&&element.hasAttribute('type')
 				&&'text'===element.getAttribute('type')))) {
 			element.value+=String.fromCharCode(options.keyCode);
 		}
 		options.type='keyup';
-		keyboard(element, options);
+		if(!(keyboard(element, options)||dispatched)) {
+			return false;
+		}
 		options.type='keypress';
-		keyboard(element, options);
+		return keyboard(element, options);
 	}
 
 	var EventSimulator = {
